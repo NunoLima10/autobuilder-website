f0bddebf5cc555440ab25243a602ab1643b45770
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2024-02-17 10:48:31 -0100

    feat: basec upload page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+
 
 function App() {
+  const [voxFile, setVoxFile] = useState(null)
+  const [paletteFile, setPaletteFile] = useState(null)
+
+  async function upload_files(event){
+    event.preventDefault()
+    const fromData = new FormData()
+    fromData.append("vox", voxFile)
+    fromData.append("palette", paletteFile)
+
+    try{
+      const response = await axios.post(" http://127.0.0.1:5000/converter",fromData,{
+        headers:{
+          'Content-Type':'multipart/form-data'
+        }
+      })
+
+      const contentType = response.headers['content-type'];
+
+      if (contentType && contentType.includes('text/plain')) {
+        
+        const url = window.URL.createObjectURL(new Blob([response.data], { type: 'text/plain' }));
+    
+        
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'MiniWorld-AutoBuilder-Script.lua'); 
+        document.body.appendChild(link);
+    
+        link.click();
+    
+        
+        window.URL.revokeObjectURL(url);
+      }
+
+    }catch(error){
+    
+      console.log(error)
+    }
+
+  }
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>MiniWorld AutoBuilder</h1>
       </header>
+
+      <form>
+        <input type="file" id="vox_file" accept=".vox" onChange={ (e) => setVoxFile(e.target.files[0])} />
+        <input type="file" id="palette_file" accept=".png" onChange={ (e) => setPaletteFile(e.target.files[0])} />
+        <button type="submit" onClick={upload_files}>Enviar</button>
+      </form>
+
+
     </div>
   );
 }
