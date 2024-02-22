3a648c1c80b0efb5ff3db15591de223ea4e80020
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2024-02-22 19:10:33 -0100

    feat: Google Analytics setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
-import Banner  from "./components/Banner/Banner";
+import Banner from "./components/Banner/Banner";
 import PaletteView from "./components/PaletteView/PaletteView";
 import VoxUploader from "./components/VoxUploader/VoxUploader";
 import DonateButton from "./components/DonateButton/DonateButton";
-
+import ReactGA from "react-ga4";
 
 function App() {
-  const [customPalette , setCustomPalette] = useState()
+  ReactGA.initialize("G-QM9R0JSXDV");
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+  const [customPalette, setCustomPalette] = useState();
+
   return (
     <div className="app">
       <Banner />
       <PaletteView customPalette={customPalette} />
       <VoxUploader onChangePalette={setCustomPalette} />
-      <DonateButton/>
+      <DonateButton />
     </div>
   );
 }
